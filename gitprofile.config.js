8a945aaebf8a45a3dfc51cde1aae5091b3084ba8
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-19 22:54:29 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -114,7 +114,8 @@ const config = {
   //   limit: 2, // How many posts to display. Max is 10.
   // },
    googleAnalytics: {
-     id: 'G-4W4YK08HPX', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+     id: 'G-4W4YK08HPX',
+   }// GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
    
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
