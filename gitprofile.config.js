f1e3ea6232730095ee8d4bb36bf9fb96868eb71a
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-19 15:42:33 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,15 +4,14 @@ const config = {
   github: {
     username: 'misbah-anwar', // Your GitHub org/user name. (Required)
     //sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
-    exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    // limit: 10, // How many projects to display.
+    // exclude: {
+    //   forks: false, // Forked projects will not be displayed if set to true.
+    //   projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
     linkedin: 'misbah-anwar',
-    twitter: 'arif_szn',
     hackerrank: 'misbahanwar2003',
     medium: 'misbah.anwar',
     website: 'https://misbah-anwar.github.io/',
@@ -139,7 +138,7 @@ const config = {
     // Available themes. To remove any theme, exclude from here.
     themes: [
       // 'light',
-       'dark',
+      // 'dark',
       // 'cupcake',
       // 'bumblebee',
       // 'emerald',
@@ -156,7 +155,7 @@ const config = {
       // 'pastel',
       // 'fantasy',
       // 'wireframe',
-      // 'black',
+       'black',
       // 'luxury',
       // 'dracula',
       // 'cmyk',
