57ee5eafb8227e426c2979870073b09d3641fa1c
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-19 16:13:30 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'misbah-anwar' // Your GitHub org/user name. (Required)
+    username: 'misbah-anwar', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
      limit: 0, // How many projects to display.
     // exclude: {
