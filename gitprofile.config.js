3c0b85569dfd8ead15aa9b431f43ae62b00c461b
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-19 21:26:50 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -122,7 +122,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'procyon',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
